e397197690b7eb89e0c9413749af6682e8922234
RaTeNo <rateno@mail.ru>
2020-06-18 17:18:55 +0300

    menu

--- js/functions.js
+++ js/functions.js
@@ -292,3 +292,42 @@ var supportsCssVars = function() {
 
     return support
 }
+
+$(window).scroll(function() {	
+	if($("body").hasClass("header_fixed"))
+	{
+		if($(window).width() > 1023)
+		{
+			if ($(window).scrollTop() > 0) {
+				$('header').addClass('fixed')
+				if($("div").is(".warning"))
+				{
+					$('header').css("top", $(".warning").innerHeight()+"px")
+				}	
+			} else {
+				$('header').removeClass('fixed')
+				if($("div").is(".warning"))
+				{
+					$('header').css("top", "0px")
+				}	
+			}
+
+		}
+		else 
+		{
+			if ($(window).scrollTop() > 0) {
+				$('.mob_header').addClass('fixed')
+				if($("div").is(".warning"))
+				{
+					$('.mob_header').css("top", $(".warning").innerHeight()+"px")
+				}	
+			} else {
+				$('.mob_header').removeClass('fixed')
+				if($("div").is(".warning"))
+				{
+					$('.mob_header').css("top", "0px")
+				}
+			}
+		}
+	}
+})
